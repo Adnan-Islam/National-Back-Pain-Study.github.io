9e1a837410dc049a5ae765de22e57ae3004a7fc8
Adnan1706 <islam.adnan07@yahoo.com>
2020-08-17 22:53:21 +0300

    few changes

--- Mock_Data.js
+++ Mock_Data.js
@@ -2,39 +2,43 @@
     // Result Display Part
 
 		var i;
-        var treatment_info = "Bellow the best result is displayed of your search based on our database (Or closest match of your search): <br><br>"; 
-        var treatment_result = [{
+        var treatment_info = "Bellow the best result is displayed of your search (or closest match of your search) based on our database : <br><br>"; 
+        var treatment_result = 
 
-            "name": "#1: Psychological treatment",
-            "reviews": "Range you specify between: 21 - 65, based on a total of 196 reviews.",
+        [{  
+            "name": "# Psychological treatment",
             "ranking": "Ranking: 1",
+            "reviews": " Decided based on a total of 296 reviews.",
             "description": "Psychological treatment of a pain patient is based on cognitive-behavioral methods that aim to improve the patient’s overall holistic approach; especially psychosocial functioning and self-care grip. KBT therapy helps the patient to calm and stabilize their cognitive-emotional and psychophysical pain experience. KBT aims to help patients also build their identity in a new way, accept the changed situation and grow into the subject of their problems."
         }, {
-            "name": "#2: Alexander Technique to change your own ways of doing business in a more present and freer direction",
-            "reviews": "Range you specify between: 28 - 82, based on a total of 65 reviews.",
+            "name": "# Alexander Technique to change your own ways of doing business in a more present and freer direction",
             "ranking": "Ranking: 2",
+            "reviews": "Decided based on a total of 165 reviews.",
             "description": "Alexander technology can be used to learn to be and act more freely and present. For the most common chronic low back pain, it has been found in an extensive study to provide long-term relief from pain by significantly reducing the number of pain days and improving functional capacity. On follow-up, it gave better results than massage, exercise counseling, or a regular doctor’s appointment. The limiting factor is that learning it requires personal guidance, but the study has already had an effect on six occasions, especially when combined with an exercise prescription. Google: 'Randomized controlled trial of Alexander technique Lessons - BMJ' or 'The potential of Alexander technique in rehabilitation thesis'."
         }, {
-            "name": "#3: Thorough Biopsychosocial Assessment",
-            "reviews": "Range you specify between: 11 - 53, based on a total of 194 reviews.",
+            "name": "# Thorough Biopsychosocial Assessment",
             "ranking": "Ranking: 3",
+            "reviews": "Decided based on a total of 112 reviews.",
             "description": "Familiarization with the pathology, prognosis and causes of low back pain from a biopsychosocial point of view. Multi-professional correction of the patient's dysfunction and incorrect lifestyle, if necessary. Motivation for independent practice.",
         }, {
-            "name": "#4: Illustrating the cause of the pain",
-            "reviews": "Range you specify between: 32 - 45, based on a total of 186 reviews.",
+            "name": "# Illustrating the cause of the pain",
             "ranking": "Ranking: 4",
+            "reviews": "Decided based on a total of 86 reviews.",
             "description": "For example, looking for a painful movement or, on the other hand, looking for a facilitative exercise together with the patient can teach the patient to study his or her own actions. The same model of logical reasoning can also be implemented in assessing the effect of drugs; the patient can keep a diary in which he observes the matter to be clarified. Experiences and observations can be used to motivate the patient. Cognitive tools are often too theoretical and far from practical ... a psychologist may observe a patient too much from a psychological perspective."  
         }, {
-            "name": "#5: Pilates",
-            "reviews": "Range you specify between: 19 - 95, based on 122 reviews.",
+            "name": "# Pilates",
             "ranking": "Ranking: 5",
+            "reviews": "Decided based on 56 reviews.",
             "description": "Works better in pain management than painkillers." 
         }];
 
+        treatment_info = treatment_info.bold().fontsize(5);
+
         for (i = 0; i < treatment_result.length; i++) {
           
-            treatment_info += treatment_result[i].name + "<br>" + treatment_result[i].reviews + "<br>" + treatment_result[i].ranking + "<br>" 
-            + treatment_result[i].description  +  "<br><p>*******************</p>";
+            treatment_info += treatment_result[i].name.bold().fontsize(4) + "<br>" + 
+            treatment_result[i].ranking.bold().fontsize(4).fontcolor("grey") + "<br>" + treatment_result[i].reviews.fontcolor("blue") 
+            + "<br>" + treatment_result[i].description +  "<br>" + "<br>";
         }
 
 function displayInfo() {
@@ -42,7 +46,7 @@ function displayInfo() {
 }
 
 function show_hide_button() {
-	   	document.getElementById("hide-desire-result").style.display = "block";
+	document.getElementById("hide-desire-result").style.display = "block";
 }
 
 
